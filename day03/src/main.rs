94ced14c6c8c1ba54687482d54b227a338d5bc90
Bruno Unna <bruno.unna@gmail.com>
2025-06-30 22:59:59 +0100

    Optimise the second part of day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -13,10 +13,13 @@ fn main() {
     }
 }
 
-fn walk(input: &str, visited: &mut HashSet<(i32, i32)>) {
+fn walk<I>(input: I, visited: &mut HashSet<(i32, i32)>)
+where
+    I: Iterator<Item = char>,
+{
     let mut position = (0, 0);
     visited.insert(position);
-    for c in input.chars() {
+    for c in input {
         let diff = match c {
             '^' => (0, -1),
             'v' => (0, 1),
@@ -33,25 +36,18 @@ fn walk(input: &str, visited: &mut HashSet<(i32, i32)>) {
 pub fn solve_part1(input: &str) -> i32 {
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
 
-    walk(input, &mut visited);
+    walk(input.chars(), &mut visited);
     visited.len().try_into().unwrap()
 }
 
 pub fn solve_part2(input: &str) -> i32 {
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
-    let mut santa_input: String = String::new();
-    let mut robo_santa_input: String = String::new();
 
-    for (i, c) in input.chars().enumerate() {
-        if i % 2 == 0 {
-            santa_input.push(c);
-        } else {
-            robo_santa_input.push(c);
-        }
-    }
+    let santa_input = input.chars().step_by(2);
+    let robo_santa_input = input.chars().skip(1).step_by(2);
 
-    walk(&santa_input, &mut visited);
-    walk(&robo_santa_input, &mut visited);
+    walk(santa_input, &mut visited);
+    walk(robo_santa_input, &mut visited);
 
     visited.len().try_into().unwrap()
 }
