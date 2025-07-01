b03260b0605ddc0a784b3ac6cd958ecb4bc85fcd
Bruno Unna <bruno.unna@gmail.com>
2025-07-01 09:18:27 +0100

    Implement and optimise for speed

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,4 @@
+use md5::{Digest, Md5};
 
 fn main() {
     let input = "yzbqklnj";
@@ -5,12 +6,39 @@ fn main() {
     println!("Day 4 Part 2: {}", solve_part2(input));
 }
 
-pub fn solve_part1(_input: &str) -> i32 {
-    0
+fn md5_sum(challenge: &[u8]) -> String {
+    let mut hasher = Md5::new();
+    hasher.update(challenge);
+    let result = hasher.finalize();
+    format!("{:x}", result)
 }
 
-pub fn solve_part2(_input: &str) -> i32 {
-    0
+fn find_number(input: &str, prefix: &str) -> i64 {
+    const MAX_I64_DIGITS: usize = 19; // For a positive i64
+    let mut challenge_buffer = String::with_capacity(input.len() + MAX_I64_DIGITS);
+
+    let mut n: i64 = 0;
+
+    loop {
+        challenge_buffer.clear();
+        challenge_buffer.push_str(input);
+        challenge_buffer.push_str(&n.to_string());
+
+        let md5 = md5_sum(challenge_buffer.as_bytes());
+        if md5.starts_with(prefix) {
+            break;
+        }
+        n += 1;
+    }
+    n
+}
+
+pub fn solve_part1(input: &str) -> i64 {
+    find_number(input, "00000")
+}
+
+pub fn solve_part2(input: &str) -> i64 {
+    find_number(input, "000000")
 }
 
 #[cfg(test)]
