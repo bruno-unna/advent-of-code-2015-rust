40f66d712f4c75381bcd4d48a5ad843851c4da1e
Bruno Unna <bruno.unna@gmail.com>
2025-07-01 15:31:12 +0100

    Implement the second part, optimise REs

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,5 +1,7 @@
 use aoc_lib::read_multiple_strings;
 use fancy_regex::Regex;
+use std::sync::LazyLock;
+
 fn main() {
     let lines_result = read_multiple_strings(5);
 
@@ -17,20 +19,34 @@ fn main() {
     }
 }
 
-fn is_nice(haystack: &str) -> bool {
-    // Compile regexes once, ideally outside the function if called repeatedly.
-    // For this example, keep them here for illustration.
-    let three_vowels_re = Regex::new(r"[aeiou].*[aeiou].*[aeiou]").unwrap();
-    let twice_in_a_row_re = Regex::new(r"(.)\1").unwrap();
-    let forbidden_combinations_re = Regex::new(r"ab|cd|pq|xy").unwrap();
+// These guys are expensive, so let's calculate them only once:
+static THREE_VOWELS_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"[aeiou].*[aeiou].*[aeiou]").unwrap());
+static FORBIDDEN_COMBINATIONS_RE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r"ab|cd|pq|xy").unwrap());
+static TWICE_IN_A_ROW_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(.)\1").unwrap());
 
-    if forbidden_combinations_re.is_match(haystack).unwrap() {
+fn is_nice(haystack: &str) -> bool {
+    if FORBIDDEN_COMBINATIONS_RE.is_match(haystack).unwrap() {
         return false;
     }
-    if three_vowels_re.is_match(haystack).unwrap() == false {
+    if THREE_VOWELS_RE.is_match(haystack).unwrap() == false {
         return false;
     }
-    if twice_in_a_row_re.is_match(haystack).unwrap() == false {
+    if TWICE_IN_A_ROW_RE.is_match(haystack).unwrap() == false {
+        return false;
+    }
+    true
+}
+
+static DOUBLE_PAIRS_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(..).*\1").unwrap());
+static SEPARATED_PAIR: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(.).\1").unwrap());
+
+fn is_very_nice(haystack: &str) -> bool {
+    if DOUBLE_PAIRS_RE.is_match(haystack).unwrap() == false {
+        return false;
+    }
+    if SEPARATED_PAIR.is_match(haystack).unwrap() == false {
         return false;
     }
     true
@@ -46,7 +62,12 @@ pub fn solve_part1(input: &[&str]) -> i32 {
 }
 
 pub fn solve_part2(input: &[&str]) -> i32 {
-    0
+    input
+        .iter()
+        .filter(|&x| is_very_nice(x))
+        .count()
+        .try_into()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -76,5 +97,25 @@ mod tests {
     #[test]
     fn test_is_nice_example_e() {
         assert_eq!(false, is_nice("dvszwmarrgswjxmb"));
+    }
+
+    #[test]
+    fn test_is_very_nice_example_a() {
+        assert_eq!(true, is_very_nice("qjhvhtzxzqqjkmpb"));
+    }
+
+    #[test]
+    fn test_is_very_nice_example_b() {
+        assert_eq!(true, is_very_nice("xxyxx"));
+    }
+
+    #[test]
+    fn test_is_very_nice_example_c() {
+        assert_eq!(false, is_very_nice("uurcxstgmygtbstg"));
+    }
+
+    #[test]
+    fn test_is_very_nice_example_d() {
+        assert_eq!(false, is_very_nice("ieodomkazucvgmuy"));
     }
 }
